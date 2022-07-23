981086fc6590cbb3c0fe0deb400dac28d5c50767
Mond1c <kornilovichmihail@gmail.com>
2022-07-23 14:16:48 +0300

    added shake sort

--- sorting.hpp
+++ sorting.hpp
@@ -11,13 +11,37 @@ namespace algorithms {
             Iterator fast = std::next(slow);
             while (fast != end) {
                 if (*fast < *slow) {
-                    auto value = *slow;
-                    *slow = *fast;
-                    *fast = value;
+                    std::swap(*fast, *slow);
                     needToSort = true;
                 }
                 fast = std::next(fast);
                 slow = std::next(slow);
+            }
+        }
+    }
+
+    template<typename Iterator>
+    void ShakeSort(Iterator begin, Iterator end) {
+        int sz = end - begin;
+        if (sz <= 1) return;
+        bool needToSort = true;
+        Iterator start = begin - 1;
+        Iterator finish = end - 1;
+        while (needToSort) {
+            needToSort = false;
+            ++start;
+            for (Iterator it = start; it < finish; ++it) {
+                if (*it > *(it + 1)) {
+                    std::swap(*it, *(it + 1));
+                    needToSort = true;
+                }
+            }
+            if (!needToSort) return;
+            --end;
+            for (Iterator it = end; it > begin; --it) {
+                if (*it < *(it - 1)) {
+                    std::swap(*it, *(it - 1));
+                }
             }
         }
     }
